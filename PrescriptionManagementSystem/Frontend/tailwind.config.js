346c3103028df9b252cae7053103963f2729a0a4
Mahashree Thanam <mahashreet@gmail.com>
2025-07-08 17:46:26 -0400

    Add files via upload

--- PrescriptionManagementSystem/Frontend/tailwind.config.js
+++ PrescriptionManagementSystem/Frontend/tailwind.config.js
@@ -7,7 +7,14 @@ export default {
     theme: {
         extend: {
             colors: {
-                primary: '#1E40AF', // Blue
+                'primary': '#1E40AF', // Blue
+            },
+            // Override or add custom gridTemplateColumns
+            gridTemplateColumns: {
+                // This overrides the default 'grid-cols-3' for example
+                // '3': 'repeat(3, minmax(0, 1fr))',
+                // Custom class: 'grid-cols-auto'
+                'auto': 'repeat(auto-fill,minmax(200px,1fr))'
             }
         },
     },
